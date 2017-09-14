3c6ff4b7d95c5ebd87ed58e74d321349b504cf81
rahil-troy <rahil.patel97@gmail.com>
2017-09-14 22:39:25 +0530

    Bug Fixes

--- iros_my.py
+++ iros_my.py
@@ -2,17 +2,20 @@ import cv2
 import numpy as np
 
 
-
 cap = cv2.VideoCapture(1)
 
 
-y,u,v = 60,176,117
-
+y,u,v = 75,179,147
 
 while True:
+
 	_,img = cap.read()
 	img_yuv = cv2.cvtColor(img,cv2.COLOR_BGR2YUV)
-	die = cv2.inRange(img_yuv, (np.array([y-45,u-30,v-30])), (np.array([y+45,u+30,v+30])))
+	
+	blur = cv2.GaussianBlur(img_yuv,(7,7),1.3)
+	median = cv2.medianBlur(blur ,5)
+
+	die = cv2.inRange(blur, (np.array([y-45,u-30,v-30])), (np.array([y+45,u+30,v+30])))
 	cv2.imshow("The Masked Image",die)
 	im_floodfill = die.copy()
  	h, w = die.shape[:2]
@@ -21,12 +24,21 @@ while True:
  
     
 	fill = cv2.bitwise_not(im_floodfill)
+	
+
 	cv2.imshow("Masked filled",fill)
  	if cv2.waitKey(25)&0xff==27:
 		break
 
-	'''contours,hierarchy = cv2.findContours(dilate,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-	if cv2.waitKey(25)&0xff==27:
-		break
-	'''	
+	contours,hierarchy = cv2.findContours(fill,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+	#abc = cv2.drawContours(img, contours, -1, (0,0,255), 1)
+	#cv2.imshow("",abc)
+	print len(contours)
+	'''for cnt in contours:
+    #if len(contours)>0:
+        #c = max(contours, key=cv2.contourArea)
+
+		x, y, w, h = cv2.boundingRect(cnt)
+		cv2.rectangle(dilation,(x,y),(x+w,y+h),[255,0,0],2)
+        #print x, y, w, h
+	'''
